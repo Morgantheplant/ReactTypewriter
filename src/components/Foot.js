7e91ee487598f57617123bf29c28a7b0a34b26ef
Morgan Plant <morgantheplant@gmail.com>
2015-09-08 16:55:29 -0700

    add keyboard events

---
+++ src/components/Foot.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var classNames = require('classnames');
+var TypewriterActions =  require('../actions/TypewriterActions');
+
+var Foot = React.createClass({
+    render: function(){
+        return (
+          <footer id="info">
+            <p>A simple keyboard build with React & Flux</p>
+              <div sytle="margin-top:20px">
+              <a href="./#/pages"><img  className="footer-pages-icon" src="./images/pages.png" alt="pages" /></a>
+              <img className="footer-github-icon" src="./images/GitHubIcon.png" alt="github icon" />
+              <img onClick={ this._clickReset() } className="footer-delete-icon" src="./images/wad.png" alt="trash it"/>     
+              </div>
+          </footer> 
+        )
+    },
+    _clickReset: function(){
+      TypewriterActions.resetAll()
+    }
+});
+
+module.exports = Foot;
+
+
